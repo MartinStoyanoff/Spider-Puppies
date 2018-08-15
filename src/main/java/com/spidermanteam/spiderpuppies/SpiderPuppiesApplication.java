9767bf17de5b7a332a6fcb463a2667bdc62da265
Martin Ivanov <stoyanoff.martin@gmail.com>
2018-08-15 15:00:16 +0300

    New project created

---
+++ src/main/java/com/spidermanteam/spiderpuppies/SpiderPuppiesApplication.java
@@ -0,0 +1,12 @@
+package com.spidermanteam.spiderpuppies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiderPuppiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiderPuppiesApplication.class, args);
+    }
+}
