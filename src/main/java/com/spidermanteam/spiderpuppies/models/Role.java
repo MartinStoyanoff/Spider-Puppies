3aaec6ab0acb793c03ac6895a78fca1f6b626a19
Martin Ivanov <stoyanoff.martin@gmail.com>
2018-08-15 19:04:41 +0300

    User and Role classes created

---
+++ src/main/java/com/spidermanteam/spiderpuppies/models/Role.java
@@ -0,0 +1,8 @@
+package com.spidermanteam.spiderpuppies.models;
+
+public class Role {
+
+    private User user;
+    private String role;
+
+}
---
+++ src/main/java/com/spidermanteam/spiderpuppies/models/User.java
@@ -0,0 +1,8 @@
+package com.spidermanteam.spiderpuppies.models;
+
+public class User {
+    private String username;
+    private String password;
+    private byte enabled;
+
+}
