0cca2712b548a60f7a77d6ccd6f557e656d361f5
Martin Ivanov <stoyanoff.martin@gmail.com>
2018-08-16 12:33:47 +0300

    User and Authorities classes connected to DB

---
+++ src/main/java/com/spidermanteam/spiderpuppies/models/User.java
@@ -0,0 +1,46 @@
+package com.spidermanteam.spiderpuppies.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "enabled")
+    private byte enabled;
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public byte getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(byte enabled) {
+        this.enabled = enabled;
+    }
+}
