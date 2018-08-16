9bdd43809a1272262f540e093322dc8da71dbab5
Martin Ivanov <stoyanoff.martin@gmail.com>
2018-08-16 13:28:21 +0300

    Hibernate configured

---
+++ src/main/java/com/spidermanteam/spiderpuppies/configuration/HibernateConfiguration.java
@@ -0,0 +1,21 @@
+package com.spidermanteam.spiderpuppies.configuration;
+
+import com.spidermanteam.spiderpuppies.models.Authorities;
+import com.spidermanteam.spiderpuppies.models.Client;
+import com.spidermanteam.spiderpuppies.models.User;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HibernateConfiguration {
+
+    public SessionFactory createSessionFactory(){
+        return new org.hibernate.cfg.Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(User.class)
+                .addAnnotatedClass(Authorities.class)
+                .addAnnotatedClass(Client.class)
+                .buildSessionFactory();
+    }
+
+}
--- src/main/java/com/spidermanteam/spiderpuppies/models/Client.java
+++ src/main/java/com/spidermanteam/spiderpuppies/models/Client.java
@@ -1,6 +1,53 @@
 package com.spidermanteam.spiderpuppies.models;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "clients")
 public class Client {
 
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "uic")
+    private String unifiedIdentificationCode;
+
+
+    public Client() {
+    }
+
+    public Client(User user, String fullName, String unifiedIdentificationCode) {
+        this.user = user;
+        this.fullName = fullName;
+        this.unifiedIdentificationCode = unifiedIdentificationCode;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUnifiedIdentificationCode() {
+        return unifiedIdentificationCode;
+    }
+
+    public void setUnifiedIdentificationCode(String unifiedIdentificationCode) {
+        this.unifiedIdentificationCode = unifiedIdentificationCode;
+    }
 }
