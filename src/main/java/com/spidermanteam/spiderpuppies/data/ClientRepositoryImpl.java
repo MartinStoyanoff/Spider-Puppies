ba4035233769b2c789e0d4f9d2303ad67e34cc40
Veselin Georgiev <veselin_georgiev@yahoo.com>
2018-08-17 13:19:16 +0300

    ClientRepositoryImpl update
    -create method created
    -findById method created
    -delete method created
    -listALl method created
    -update method created

--- src/main/java/com/spidermanteam/spiderpuppies/data/ClientRepositoryImpl.java
+++ src/main/java/com/spidermanteam/spiderpuppies/data/ClientRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.spidermanteam.spiderpuppies.data;
 
 import com.spidermanteam.spiderpuppies.data.base.ClientRepository;
+import com.spidermanteam.spiderpuppies.data.base.GenericRepository;
 import com.spidermanteam.spiderpuppies.models.Authorities;
 import com.spidermanteam.spiderpuppies.models.Client;
 import com.spidermanteam.spiderpuppies.models.User;
@@ -13,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-public class ClientRepositoryImpl implements ClientRepository {
+public class ClientRepositoryImpl implements GenericRepository<Client> {
 
     private SessionFactory sessionFactory;
 
@@ -24,33 +25,71 @@ public class ClientRepositoryImpl implements ClientRepository {
 
 
     @Override
-    public void addClient(Client client) {
+    public void create(Client client) {
         User user = client.getUser();
-        Authorities authorities = new Authorities(user.getUsername(),"ROLE_ADMIN");
-        try(Session session = sessionFactory.openSession()){
+        Authorities authorities = new Authorities(user.getUsername(), "ROLE_ADMIN");
+        try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
             session.save(user);
             session.save(authorities);
             session.save(client);
             session.getTransaction().commit();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
     }
 
     @Override
-    public List listAllClients() {
-        List clients = new ArrayList<>();
-        try(Session session = sessionFactory.openSession()){
+    public List<Client> listAll() {
+        List<Client> clients = new ArrayList<>();
+        try (Session session = sessionFactory.openSession()) {
             session.beginTransaction();
             clients = session.createQuery("from Client").list();
             session.getTransaction().commit();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         return clients;
+    }
+
+    @Override
+    public Client findById(int id) {
+        Client client = new Client();
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            client = session.get(Client.class, id);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return client;
+    }
+
+
+    @Override
+    public void update(Client client) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.update(client);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            Client client = session.get(Client.class, id);
+            session.delete(client);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
