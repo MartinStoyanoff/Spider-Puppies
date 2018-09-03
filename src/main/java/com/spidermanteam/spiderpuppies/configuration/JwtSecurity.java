17f687678a1425764c2d2351cd6a189c8a660d47
Veselin Georgiev <veselin_georgiev@yahoo.com>
2018-09-03 20:55:10 +0300

    JwtSecurity implemented

---
+++ src/main/java/com/spidermanteam/spiderpuppies/configuration/JwtSecurity.java
@@ -0,0 +1,68 @@
+package com.spidermanteam.spiderpuppies.configuration;
+
+import com.spidermanteam.spiderpuppies.security.JwtAuthenticationEntryPoint;
+import com.spidermanteam.spiderpuppies.security.JwtAuthenticationProvider;
+import com.spidermanteam.spiderpuppies.security.JwtAuthenticationTokenFilter;
+import com.spidermanteam.spiderpuppies.security.JwtSuccessHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Collections;
+
+@EnableGlobalMethodSecurity(prePostEnabled  = true, securedEnabled = true)
+@EnableWebSecurity
+@Configuration
+public class JwtSecurity extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    private JwtAuthenticationProvider authenticationProvider;
+    @Autowired
+    private JwtAuthenticationEntryPoint entryPoint;
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        return new ProviderManager(Collections.singletonList(authenticationProvider));
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public JwtAuthenticationTokenFilter authenticationTokenFilter() {
+        JwtAuthenticationTokenFilter filter = new JwtAuthenticationTokenFilter();
+        filter.setAuthenticationManager(authenticationManager());
+        filter.setAuthenticationSuccessHandler(new JwtSuccessHandler());
+        return filter;
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.cors()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeRequests().antMatchers("**/secure/**").authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(entryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(authenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.headers().cacheControl();
+
+    }
+}
