86ed684ff7542270669059f853f1d044534a3e95
Veselin Georgiev <veselin_georgiev@yahoo.com>
2018-09-08 21:28:59 +0300

    AdminServiceAllTest new tests added

--- src/test/java/com/spidermanteam/spiderpuppies/services/admin/AdminServiceAllTests.java
+++ src/test/java/com/spidermanteam/spiderpuppies/services/admin/AdminServiceAllTests.java
@@ -2,6 +2,7 @@ package com.spidermanteam.spiderpuppies.services.admin;
 
 import com.spidermanteam.spiderpuppies.data.base.AdminRepository;
 import com.spidermanteam.spiderpuppies.models.Admin;
+import com.spidermanteam.spiderpuppies.models.User;
 import com.spidermanteam.spiderpuppies.services.AdminServiceImpl;
 import org.junit.Assert;
 import org.junit.Before;
@@ -24,11 +25,14 @@ public class AdminServiceAllTests {
   @Mock
   AdminRepository adminRepository;
 
+
   private AdminServiceImpl adminService;
   private BCryptPasswordEncoder passwordEncoder;
 
+
   @Before
   public void beforeTest() {
+    passwordEncoder = new BCryptPasswordEncoder();
     adminService = new AdminServiceImpl(adminRepository, passwordEncoder);
   }
 
@@ -99,5 +103,46 @@ public class AdminServiceAllTests {
     doNothing().when(adminRepository).update(isA(Admin.class));
     adminService.updateAdmin(admin);
     verify(adminRepository, times(1)).update(admin);
+  }
+
+  @Test
+  public void changeAdminPassword_whenPasswordUpdateInfoIsPresented_ShouldChangeAdminPassword() {
+    User user = new User();
+    user.setId(1L);
+    String passEncoded = passwordEncoder.encode("123");
+    user.setPassword(passEncoded);
+    Admin admin = new Admin(user, "www@goole.bg", 1);
+    admin.setId(1);
+
+    String adminId = "1";
+    String oldPassword = "123";
+    String newPassword = "12345";
+
+    List<String> updateInfoList = new ArrayList<>();
+    updateInfoList.add(adminId);
+    updateInfoList.add(oldPassword);
+    updateInfoList.add(newPassword);
+
+    when(adminRepository.findById(1)).thenReturn(admin);
+    doNothing().when(adminRepository).updateAdminPassword(isA(Admin.class));
+    adminService.changeAdminPassword(updateInfoList);
+
+    Assert.assertTrue(passwordEncoder.matches(newPassword, admin.getUser().getPassword()));
+
+  }
+
+  @Test
+  public void findAdminByUserUsername_whenUsernameIsPresented_ShouldReturnAdmin() {
+    User user = new User();
+    user.setUsername("username");
+    Admin admin = new Admin(user, "email", 1);
+    admin.setId(1);
+
+    when(adminRepository.findAdminByUserUsername("username")).thenReturn(admin);
+    Admin actualAdmin = adminService.findAdminByUserUsername("username");
+
+    Assert.assertEquals(admin.getId(), actualAdmin.getId());
+
+
   }
 }
